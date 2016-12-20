00940ebf3fc346edd98210abb5d21d6b281188bc
Josh Pann <josh.pann@example.com>
2016-12-20 15:29:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,11 +7,15 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    ### Instance variables are ONLY passed from the controller method where they are created to the view that is rendered, not between controller methods.
     erb :reversed
   end
 
   get '/friends' do
-    #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
